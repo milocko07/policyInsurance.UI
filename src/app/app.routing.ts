6cc5312cf2ff770189267456555ed6d71070ab88
milocko07 <37205551+milocko07@users.noreply.github.com>
2019-09-01 16:41:57 -0500

    template seed with jwt validation.

---
+++ src/app/app.routing.ts
@@ -0,0 +1,96 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Import Containers
+import { DefaultLayoutComponent } from './containers';
+
+import { P404Component } from './views/error/404.component';
+import { P500Component } from './views/error/500.component';
+import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './views/register/register.component';
+
+import { AuthGuard } from './guards/auth-guard.service';
+
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'dashboard',
+    pathMatch: 'full'
+  },
+  {
+    path: '404',
+    component: P404Component,
+    data: {
+      title: 'Page 404'
+    }
+  },
+  {
+    path: '500',
+    component: P500Component,
+    data: {
+      title: 'Page 500'
+    }
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+    data: {
+      title: 'Login Page'
+    }
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    data: {
+      title: 'Register Page'
+    }
+  },
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    data: {
+      title: 'Home', canActivate: [AuthGuard]
+    },
+    children: [
+      {
+        path: 'base',
+        loadChildren: () => import('./views/base/base.module').then(m => m.BaseModule)
+      },
+      {
+        path: 'buttons',
+        loadChildren: () => import('./views/buttons/buttons.module').then(m => m.ButtonsModule), canActivate: [AuthGuard]
+      },
+      {
+        path: 'charts',
+        loadChildren: () => import('./views/chartjs/chartjs.module').then(m => m.ChartJSModule), canActivate: [AuthGuard]
+      },
+      {
+        path: 'dashboard',
+        loadChildren: () => import('./views/dashboard/dashboard.module').then(m => m.DashboardModule), canActivate: [AuthGuard]
+      },
+      {
+        path: 'icons',
+        loadChildren: () => import('./views/icons/icons.module').then(m => m.IconsModule), canActivate: [AuthGuard]
+      },
+      {
+        path: 'notifications',
+        loadChildren: () => import('./views/notifications/notifications.module').then(m => m.NotificationsModule), canActivate: [AuthGuard]
+      },
+      {
+        path: 'theme',
+        loadChildren: () => import('./views/theme/theme.module').then(m => m.ThemeModule), canActivate: [AuthGuard]
+      },
+      {
+        path: 'widgets',
+        loadChildren: () => import('./views/widgets/widgets.module').then(m => m.WidgetsModule), canActivate: [AuthGuard]
+      }
+    ]
+  },
+  { path: '**', component: P404Component }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
